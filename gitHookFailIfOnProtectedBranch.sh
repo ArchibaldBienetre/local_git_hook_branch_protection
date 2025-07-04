7c20e1d08d1c0ecc11a01d89bf441900da34b148
Robert G <bluewhobert@mailbox.org>
2025-07-04 14:40:31 +0200

    feat: add the actual script
    This is what it's all about :)

---
+++ gitHookFailIfOnProtectedBranch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Use this as local pre-push hook to prevent accidental pushes to main, master, develop
+# To override, add the " --no-verify" flag to your push command.
+#
+# To associate it with _all_ your git repos: 
+# * Create a directory "${HOME}/.githooks"
+# mkdir "${HOME}/.githooks"
+# * Set that directory as your hooks directory:
+# git config --global core.hooksPath "${HOME}/.githooks"
+# * Move this file into your hooks directory, and rename it to "pre-push" (or, set a symbolic link "pre-push" in your git hooks directory)
+# cp gitHookFailIfOnProtectedBranch.sh "${HOME}/.githooks/pre-push
+
+echo "### GIT 'pre-push' hook: Checking branch name... ###"
+local_branch=$(git rev-parse --abbrev-ref HEAD)
+
+protected_branch_regex="^(main|master|develop)$"
+
+if [[ "${local_branch}" =~ ${protected_branch_regex} ]]
+then
+    echo "### FAILED 'pre-push' hook: Trying to push to a protected branch: ${local_branch}. Add ' --no-verify' flag if you are absolutely sure."
+    exit 1
+fi
+
+echo "### DONE 'pre-push' hook: Branch name ${local_branch} is OK. ###"
+exit 0
