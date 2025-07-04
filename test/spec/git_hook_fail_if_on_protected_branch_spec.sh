0a19e77ed9c43aaaa97cb00ba49956ff9c2edd93
Robert G <bluewhobert@mailbox.org>
2025-07-04 20:50:05 +0200

    chore: refactor
    Use parameterized test design
    (less verbose)

--- test/spec/git_hook_fail_if_on_protected_branch_spec.sh
+++ test/spec/git_hook_fail_if_on_protected_branch_spec.sh
@@ -11,50 +11,31 @@ Describe "git_hook_fail_if_on_protected_branch.sh"
     End
 
     Describe "On protected branch"
-      It "fails on branch 'main'"
-        export BRANCH_NAME="main"
-        When I run script "../git_hook_fail_if_on_protected_branch.sh"
-        The status should be failure
-        The output should include "### FAILED 'pre-push' hook: Trying to push to a protected branch: 'main'."
+      Parameters
+        "main"
+        "master"
+        "develop"
       End
-      It "fails on branch 'master'"
-        export BRANCH_NAME="master"
+      Example "fails on branch '$1'"
+        export BRANCH_NAME="$1"
         When I run script "../git_hook_fail_if_on_protected_branch.sh"
         The status should be failure
-        The output should include "### FAILED 'pre-push' hook: Trying to push to a protected branch: 'master'."
-      End
-      It "fails on branch 'develop'"
-        export BRANCH_NAME="develop"
-        When I run script "../git_hook_fail_if_on_protected_branch.sh"
-        The status should be failure
-        The output should include "### FAILED 'pre-push' hook: Trying to push to a protected branch: 'develop'."
+        The output should include "### FAILED 'pre-push' hook: Trying to push to a protected branch: '$1'."
       End
     End
 
     Describe "On unprotected branch"
-      It "succeeds on branch 'other'"
-        export BRANCH_NAME="other"
-        When I run script "../git_hook_fail_if_on_protected_branch.sh"
-        The status should be success
-        The output should include "### DONE 'pre-push' hook: Branch name 'other' is OK. ###"
+      Parameters
+        "other"
+        "release"
+        "hotfix/1.2.3"
+        "feature/JIRA-12345-my-feature-branch"
       End
-      It "succeeds on branch 'release'"
-        export BRANCH_NAME="release"
+      Example "succeeds on branch '$1'"
+        export BRANCH_NAME="$1"
         When I run script "../git_hook_fail_if_on_protected_branch.sh"
         The status should be success
-        The output should include "### DONE 'pre-push' hook: Branch name 'release' is OK. ###"
-      End
-      It "succeeds on branch 'hotfix/1.2.3'"
-        export BRANCH_NAME="hotfix/1.2.3"
-        When I run script "../git_hook_fail_if_on_protected_branch.sh"
-        The status should be success
-        The output should include "### DONE 'pre-push' hook: Branch name 'hotfix/1.2.3' is OK. ###"
-      End
-      It "succeeds on branch 'feature/JIRA-12345-my-feature-branch'"
-        export BRANCH_NAME="feature/JIRA-12345-my-feature-branch"
-        When I run script "../git_hook_fail_if_on_protected_branch.sh"
-        The status should be success
-        The output should include "### DONE 'pre-push' hook: Branch name 'feature/JIRA-12345-my-feature-branch' is OK. ###"
+        The output should include "### DONE 'pre-push' hook: Branch name '$1' is OK. ###"
       End
     End
   End
